af327f9acde6e025218408c3b4a2129b78f561be
Saulompr99 <saulompr@gmail.com>
2022-02-23 09:05:06 +0000

    Agrega suma de tres parametros

--- Main.java
+++ Main.java
@@ -15,5 +15,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    private int suma_tres(int a, int b, int c) {
+        return a+b+c;
+    }
 }
